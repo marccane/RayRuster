458a262f09cec1b31affc522fc8d891152fd93b3
Marc Cane <marc00077@hotmail.com>
2020-08-04 16:50:24 +0200

    Fixed vertical mirroring, cleanup

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ use rayruster::raytracing::{Ray2, Vec3, Color2, Point32, Intersectable, Intersec
 use rayruster::figures::*;
 use rayruster::{materials::*, settings, utils::*};
 
+use rayon::prelude::*;
+
 fn write_color(image_ascii_data: &mut String, pixel_color: &Color2, samples_per_pixel: i32) {
 
     let scale = 1.0 / samples_per_pixel as f32;
@@ -58,8 +60,6 @@ fn ray_color(r: Ray2, world: &dyn Intersectable, depth: i32) -> Color2 {
     }
 }
 
-use rayon::prelude::*;
-
 fn main() -> std::io::Result<()> {
 
     // Settings retreive from run parameters.
@@ -74,8 +74,8 @@ fn main() -> std::io::Result<()> {
 
     //image
     const ASPECT_RATIO: f32 = 16.0 / 9.0;
-    const IMAGE_WIDTH: i32 = 384;
-    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as i32;
+    const IMAGE_WIDTH: usize = 384;
+    const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as usize;
     const SAMPLES_PER_PIXEL: i32 = 50;
     const MAX_DEPTH: i32 = 10;
     
@@ -95,19 +95,18 @@ fn main() -> std::io::Result<()> {
     let camera = Camera::new();
 
     //misc
-    let mut rng = rand::thread_rng();
+    let mut pixel_col_vec = vec![(0, None); NUM_PIXELS];
     const NUM_PIXELS: usize = (IMAGE_WIDTH * IMAGE_HEIGHT) as usize;
-    let mut pixel_col_vec = vec![(0, None); NUM_PIXELS]; //i32, Option::<Color2>
     
     for i in 0..NUM_PIXELS {
         pixel_col_vec[i] = (i, None);
     }
-    pixel_col_vec[0] = (0, Some(Color2::new(0.0,0.0,0.0)));
+    //pixel_col_vec[0] = (0, Some(Color2::new(0.0,0.0,0.0)));
     
     pixel_col_vec.par_iter_mut().for_each(|t| {
         let (pixelidx, _) = *t;
-        let i = pixelidx as i32 % IMAGE_WIDTH;
-        let j = pixelidx as i32/ IMAGE_WIDTH;
+        let i = pixelidx % IMAGE_WIDTH;
+        let j = pixelidx / IMAGE_WIDTH;
         let mut rng = rand::thread_rng();
         
         let mut pixel_color = Color2::new(0.0,0.0,0.0);
@@ -119,34 +118,14 @@ fn main() -> std::io::Result<()> {
         }
         
         *t = (0, Some(pixel_color));
-        
-        //let mut rng = thread_rng();
-        //*p = (0..5).map(|_| rng.sample(&Alphanumeric)).collect()
-        
     });
-
-    /*for j in (0..IMAGE_HEIGHT).rev() {
-        //println!("{}", j);
-        //println!("\rScanlines remaining: {} ", j); //commented out while the \r thing doesn't work
-        std::io::stdout().flush().expect("error flushing stdout");
-        for i in 0..IMAGE_WIDTH {
-
-            let mut pixel_color = Color2::new(0.0,0.0,0.0);
-            for s in 0..SAMPLES_PER_PIXEL {
-                let u = (i as f32 + rng.gen::<f32>()) / (IMAGE_WIDTH - 1) as f32;
-                let v = (j as f32 + rng.gen::<f32>()) / (IMAGE_HEIGHT - 1) as f32;
-                let r = camera.get_ray(u,v);
-                pixel_color += ray_color(r, &world, MAX_DEPTH);
-            }
-
-            write_color(&mut image_ascii_data, &pixel_color, SAMPLES_PER_PIXEL);
-            raytraced_color_buffer.push(pixel_color);
-        }
-    }*/
     
-    for tpl in pixel_col_vec {
-        let (_, opt_pix_col) = tpl;
-        write_color(&mut image_ascii_data, &opt_pix_col.unwrap(), SAMPLES_PER_PIXEL);
+
+    for i in (0..IMAGE_HEIGHT).rev() {
+        for j in 0..IMAGE_WIDTH {
+            let (_, opt_pix_col) = pixel_col_vec[i*IMAGE_WIDTH + j];
+            write_color(&mut image_ascii_data, &opt_pix_col.unwrap(), SAMPLES_PER_PIXEL);
+        }
     }
 
     file.write_all(image_ascii_data.as_bytes())?;
